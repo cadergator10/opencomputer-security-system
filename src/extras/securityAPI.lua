511646aee022d1424c6f12888390244b9c2cb03e
Cade <kisercade@gmail.com>
2022-03-22 12:56:21 +0000

    eh no ideas

--- src/extras/securityAPI.lua
+++ src/extras/securityAPI.lua
@@ -1,6 +1,10 @@
 --The purpose of this is to provide an easy library to make your own programs that work with the security system.
 --This only works with 2.#.# systems
 
-function checkPass(msg)
+local securityData = {}
+
+function securityData.checkPass(msg)
     
 end
+
+return securityData
